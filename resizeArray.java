b68126f71830a375413f5e35b6b3f1d93b8462bf
jeetusingh247 <jeetusingh11010@gmail.com>
2024-10-11 21:51:50 +0530

    none

--- resizeArray.java
+++ resizeArray.java
@@ -1,25 +1,33 @@
 public class resizeArray {
-    // --> Algorithm to resize an array
-    public static void resize(int[] arr, int capacity){
+
+    // Algorithm to print an array
+    public void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n ; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    // algo to resize array
+    public int[] resize(int[] arr, int capacity){
         int[] temp = new int[capacity];
         for (int i = 0; i < arr.length; i++) {
             temp[i] = arr[i];
         }
         arr = temp;
+        return arr;
     }
-    // --> Algorithm to print an array
-    public static void printArray(int[] arr) {
-        int n = arr.length;
-        for(int i = 0; i < n; i++) {
-            System.out.println(arr[i] + " ");
-        }
-        System.out.println();
-    }
-    
+
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6, 7};
-        System.out.println("before resize" + arr.length);
-        resize(arr, 9);
-        System.out.println("after resize" + arr.length);
+        resizeArray arr1 = new resizeArray();
+        int[] original = new int[] {5, 1, 2, 6, 7};
+        System.out.println("Size of Original Array is : " + original.length);
+        original = arr1.resize(original, 7);
+        System.out.println("size of array after modified : " + original.length);
+
     }
+
+
+
+
 }
