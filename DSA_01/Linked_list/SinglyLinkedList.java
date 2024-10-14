7ecff829272b2299604cc5b452684c613a192505
jeetusingh247 <jeetusingh11010@gmail.com>
2024-10-14 19:00:21 +0530

    Linked List

---
+++ DSA_01/Linked_list/SinglyLinkedList.java
@@ -0,0 +1,27 @@
+package DSA_01.Linked_list;
+
+public class SinglyLinkedList {
+    
+    private ListNode head;
+
+    private static class ListNode {
+        private int data; // Generic Type
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode fourth = new ListNode(11);
+        sll.head.next = second; // 10 --> 1
+        second.next = third; // 10 --> 1 --> 8
+        third.next = fourth; // 10 --> 1 --> 8 --> 11 --> null
+    }
+}
