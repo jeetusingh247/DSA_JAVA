c6a1ea2fcd4ba2bb789716d6e85269e0d097fd0c
jeetusingh247 <jeetusingh11010@gmail.com>
2024-10-14 19:07:25 +0530

    modified structure

--- Core_Java/Java_Basics/classLoader.java
+++ Core_Java/Java_Basics/classLoader.java
@@ -1,5 +1,6 @@
 public class classLoader {
     public static void main(String[] args) {
+        @SuppressWarnings("rawtypes")
         Class c = classLoader.class;
         System.out.println(c.getClassLoader());
         System.out.println(String.class.getClassLoader());
