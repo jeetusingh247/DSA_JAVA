5a926fd40a75c9e7cb44667f392f73174b66de12
jeetusingh247 <jeetusingh11010@gmail.com>
2024-10-14 12:46:03 +0530

    updated operators

--- Core_Java/Java_Basics/operators.java
+++ Core_Java/Java_Basics/operators.java
@@ -100,6 +100,62 @@ public class operators {
         System.out.println();
 
         // (5) Java OR Operator Example: Logical || and Bitwise |
+        // Example 1
+        int x2 = 10;
+        int y2 = 5;
+        int z2 = 20;
+        System.out.println(x2 > y2 || x2 < z2); // true || true = true
+        System.out.println(x2 > y2 | x2 < z2); // true | true = true
+        System.out.println(); // | VS |
+        System.out.println(x2 > y2 || x2++ < z2); // true || true = true
+        System.out.println(x2); // 10 because second condition is not checked
+        System.out.println();
+        System.out.println(x2 > y2 | x2++ < z2); // true | true = true
+        System.out.println(x2); // 11 because second condition is also checked
+        System.out.println();
+
+        // (6) Java Ternary Operator
+        // Example 1
+        int x3 = 2;
+        int y3 = 5;
+        int min = (x3 < y3) ? x3 : y3; // --> ternary operator which takes three operands
+        System.out.println(min); // 2
+
+        // (7) Java Assignment Operator
+        // Example 1
+        int a7 = 20;
+        int b7 = 30;
+        a += 4; // a7 = a7 + 4 (a7 = 20 + 4)
+        b -= 4; // b7 = b7 - 4 (b7 = 30 - 4)
+        System.out.println(a7);
+        System.out.println(b7);
+        System.out.println();
+
+        // Example 2
+        int d2 = 20;
+        d2 += 4; // 20 + 4
+        System.out.println(d2); // 24
+        d2 -= 4; // 24 - 4
+        System.out.println(d2); // 20
+        d2 *= 2; // 20 * 2
+        System.out.println(d2); // 40
+        d2 /= 10; // 40 / 10
+        System.out.println(d2); // 4
+        System.out.println();
+
+        // Example 3 --> Adding Sort
+        // short s = 10;
+        // short s2 = 10;
+        // s = s + s2; // Compile time error because 10 + 10 = 20 now integer
+        // System.out.println(s);
+
+        // After TypeCast
+        short s = 10;
+        short s2 = 10;
+        s = (short)(s + s2); // 20 which is now converted to short
+        System.out.println(s);
+
+
 
     }
 }
