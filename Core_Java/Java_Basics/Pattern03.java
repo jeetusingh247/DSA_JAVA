fec3480d6b75385f1ed0c929e3c92bab73063199
jeetusingh247 <jeetusingh11010@gmail.com>
2024-10-27 17:24:32 +0530

    Char pattern

---
+++ Core_Java/Java_Basics/Pattern03.java
@@ -0,0 +1,41 @@
+// Patterns Characters
+import java.util.Scanner;
+
+
+public class Pattern03 {
+    // Right Triangle Alphabetic Pattern
+    public static void main(String[] args) {
+        // Characters Patterns
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = in.nextInt();
+
+        // Right Triangle Alphabetic Pattern - 01
+        // char ch = 'A';
+        // for (int i = 1; i <= rows; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //     System.out.print(ch + " ");
+        //     }
+        //     ch++;
+        //     System.out.println();
+        // }
+        
+        // Repeating Alphabets Pattern - 02
+        // for (int i = 0; i < rows; i++) {
+        //     char alphabet = 'A';
+        //     for (int j = 0; j < rows; j++) {
+        //     if (j == 0 || i == j || i + j == rows - 1) {
+        //         System.out.print(alphabet + " ");
+        //     } else {
+        //         System.out.print(alphabet + " ");
+        //     }
+        //     alphabet++;
+        //     }
+        //     System.out.println();
+        // }
+
+
+        
+    }
+}
